02b4ecb59954a2ce138513d8e8bdb801fb6e7a88
Drikus-Bothma <231155@virtualwindow.co.za>
2023-11-07 01:03:12 +0200

    Semi final project

--- scripts/library.js
+++ scripts/library.js
@@ -1,74 +1,89 @@
-// function getMovieDetails(movie_id) {
-//     const apiUrl = `https://api.themoviedb.org/3/movie/{movie_id}`;
+$(document).ready(function(){
   
-//     $.ajax({
-//       url: apiUrl,
-//       method: 'GET',
-//       dataType: 'json',
-//       success: function(data) {
-//         const movie = data.movies[0];
+    console.log("Hello");
 
-//         console.log(movie);
-        
-//         if (movie) {
-//           $('#movieTitle').text(movie.strMovie);
-//           $('#movieDetails').text(movie.strMovieDetails);
+    var acc = document.getElementsByClassName("accordion");
+var i;
 
-//         } else {
-//           // Handle the case where the cocktail is not found
-//         }
-//       },
-//       error: function(error) {
-//         console.error('Error:', error);
-//       }
-//     });
-//   }
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    
+    this.classList.toggle("active");
 
-  const apiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=93a6dc3e';
+    
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+let movie = [];
+
+const apiUrl = "https://api.themoviedb.org/3/movie/popular?api_key=256a35c109969ca8fc077726003b1ca4&language=en-US&page=2";
 
 $.ajax({
   url: apiUrl,
   method: 'GET',
   dataType: 'json',
   success: function(data) {
-    if (data.Response === 'True') {
-      // Map the results to a JavaScript object
-      const movies = data.Search.map(movie => ({
-        id: movie.imdbID,
-        name: movie.Title,
-        image: movie.Poster,
-      }));
-
-      displayMovies(movies);
-    } else {
-      console.error('Error:', data.Error);
-    }
+    
+    movie = data.results;
+    console.log(movie)
+    displayMovies(movie);
+    
   },
   error: function(error) {
     console.error('Error:', error);
   }
-});
+}).done(function (){
+
+})
 
 const moviesContainer = $('#moviesContainer');
 
-function displayMovies(movies) {
-  $.each(movies, function(index, movie) {
-    const card = $(
+function displayMovies(movie) {
+  console.log('Displaying movies:', movie);
+  $.each(movie, function(index, movie) {
+
+    var posterBaseUrl = "https://image.tmdb.org/t/p/w500"
+    var moviePosterPath = movie.poster_path;
+    let imageSrc = posterBaseUrl + moviePosterPath;
+   // $("#MovieCover").attr("src", posterBaseUrl + moviePosterPath);
+    const card = $(`
       <div class="col-md-2 md-3">
         <div class="card">
-          <img src="${movie.image}" class="card-img-top" alt="${movie.name}"></img>
+          <img src="${imageSrc}" class="card-img-top" alt="${movie.name}">
           <div class="card-body">
-            <h5 class="card-title">${movie.name}</h5>
+            <h5 class="card-title">${movie.title}</h5>
           </div>
         </div>
       </div>
-    );
+    `);
 
     // Add a click event to navigate to a new page with the movie ID in the URL
     card.click(function() {
-      window.location.href = movie.html?id=$:{movieId};
+      window.location.href = `individual.html?id=${movie.id}`;
     });
 
     moviesContainer.append(card);
   });
 }
+});
