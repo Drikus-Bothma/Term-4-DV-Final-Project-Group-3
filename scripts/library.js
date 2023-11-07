c7982362c2f9cc65ebca1c5114a160440e5e2d6c
Drikus-Bothma <125968717+Drikus-Bothma@users.noreply.github.com>
2023-11-07 09:55:00 +0200

    Update library.js

---
+++ scripts/library.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+  
+    console.log("Hello");
+
+    var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    
+    this.classList.toggle("active");
+
+    
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+let movie = [];
+
+const apiUrl = "https://api.themoviedb.org/3/movie/popular?api_key=256a35c109969ca8fc077726003b1ca4&language=en-US&page=2";
+
+$.ajax({
+  url: apiUrl,
+  method: 'GET',
+  dataType: 'json',
+  success: function(data) {
+    
+    movie = data.results;
+    console.log(movie)
+    displayMovies(movie);
+    
+  },
+  error: function(error) {
+    console.error('Error:', error);
+  }
+}).done(function (){
+
+})
+
+const moviesContainer = $('#moviesContainer');
+
+function displayMovies(movie) {
+  console.log('Displaying movies:', movie);
+  $.each(movie, function(index, movie) {
+
+    var posterBaseUrl = "https://image.tmdb.org/t/p/w500"
+    var moviePosterPath = movie.poster_path;
+    let imageSrc = posterBaseUrl + moviePosterPath;
+   // $("#MovieCover").attr("src", posterBaseUrl + moviePosterPath);
+    const card = $(`
+      <div class="col-md-2 md-3">
+        <div class="card">
+          <img src="${imageSrc}" class="card-img-top" alt="${movie.name}">
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+          </div>
+        </div>
+      </div>
+    `);
+
+    // Add a click event to navigate to a new page with the movie ID in the URL
+    card.click(function() {
+      window.location.href = `individual.html?id=${movie.id}`;
+    });
+
+    moviesContainer.append(card);
+  });
+}
+});
