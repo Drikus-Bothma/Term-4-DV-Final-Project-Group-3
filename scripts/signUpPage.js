506c8e40af5105e155633a898c17d21b7a9ba989
Drikus-Bothma <125968717+Drikus-Bothma@users.noreply.github.com>
2023-11-07 09:55:39 +0200

    Update signUpPage.js

--- scripts/signUpPage.js
+++ scripts/signUpPage.js
@@ -1,1 +1,27 @@
+function validateForm() {
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
 
+    var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/;
+
+    if (!passwordRegex.test(password)) {
+        alert("Password must contain at least one uppercase letter, one lowercase letter, and one number");
+        return false;
+    }
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match");
+        return false;
+    }
+
+    // Store data in localStorage (if needed)
+    localStorage.setItem("username", username);
+    localStorage.setItem("email", email);
+
+    // Use 'window.location.replace' for better redirection
+    window.location.replace('home.html');
+
+    return false; // Prevent the default form submission
+}
